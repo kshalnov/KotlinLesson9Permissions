e442165fe052f97a05fd04c8a58bd90ef54c2a21
Kirill Shalnov <borhammere@gmail.com>
2021-11-28 20:11:23 +0300

    enable view binding

--- app/src/main/java/ru/gb/course1/kotlinlesson9permissions/MainActivity.kt
+++ app/src/main/java/ru/gb/course1/kotlinlesson9permissions/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.gb.course1.kotlinlesson9permissions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.gb.course1.kotlinlesson9permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
