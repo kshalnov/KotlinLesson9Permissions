3b2c56ae6197638c6bb2fa1110db3bb813075cf0
Kirill Shalnov <borhammere@gmail.com>
2021-11-28 21:03:35 +0300

    Permissions sample

--- app/src/main/java/ru/gb/course1/kotlinlesson9permissions/MainActivity.kt
+++ app/src/main/java/ru/gb/course1/kotlinlesson9permissions/MainActivity.kt
@@ -1,17 +1,54 @@
 package ru.gb.course1.kotlinlesson9permissions
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
 import ru.gb.course1.kotlinlesson9permissions.databinding.ActivityMainBinding
+
+private const val CONTACTS_PERMISSION_REQUEST_CODE = 1212
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val targetPermission: String = Manifest.permission.READ_CONTACTS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.permissionNameTextView.text = targetPermission
 
+        binding.checkButton.setOnClickListener {
+            val permissionResult = checkSelfPermission(targetPermission)
+            val hasPermission = permissionResult == PermissionChecker.PERMISSION_GRANTED
+            binding.permissionCheckbox.isChecked = hasPermission
+            binding.rationalyCheckbox.isChecked =
+                shouldShowRequestPermissionRationale(targetPermission)
+        }
+
+        binding.requestButton.setOnClickListener {
+            requestPermissions(
+                arrayOf(targetPermission),
+                CONTACTS_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        binding.resultTextView.text =
+            PermissionResult(requestCode, permissions, grantResults).toString()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
+
+private data class PermissionResult(
+    val requestCode: Int,
+    val permissions: Array<out String>,
+    val grantResults: IntArray
+)
